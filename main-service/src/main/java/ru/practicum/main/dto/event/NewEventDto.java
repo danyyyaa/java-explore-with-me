219032d41ffbb491c329f053f4a17c47f14c567c
danya <danissim00o@yandex.ru>
2023-08-28 15:28:25 +0700

    refactor: delete commented code

--- main-service/src/main/java/ru/practicum/main/dto/event/NewEventDto.java
+++ main-service/src/main/java/ru/practicum/main/dto/event/NewEventDto.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 import ru.practicum.main.dto.location.LocationDtoCoordinates;
 
 import javax.validation.constraints.NotBlank;
@@ -20,14 +21,14 @@ import static ru.practicum.main.util.Constant.TIME_PATTERN;
 public class NewEventDto {
 
     @NotBlank
-    //@Length(min = 20, max = 2000)
+    @Length(min = 20, max = 2000)
     private String annotation;
 
     @NotNull
     private Long category;
 
     @NotBlank
-    //@Length(min = 20, max = 7000)
+    @Length(min = 20, max = 7000)
     private String description;
 
     @JsonFormat(pattern = TIME_PATTERN)
@@ -46,6 +47,6 @@ public class NewEventDto {
     private Boolean requestModeration;
 
     @NotBlank
-    //@Length(min = 3, max = 120)
+    @Length(min = 3, max = 120)
     private String title;
 }
